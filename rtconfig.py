84fdb910e02f87ad244ca633a6b84b23fd1859e2
Daniel Doña <daniel-dona@users.noreply.github.com>
2025-09-14 00:00:18 +0200

    Change EXEC_PATH for docker builder

--- rtconfig.py
+++ rtconfig.py
@@ -17,7 +17,7 @@ if os.getenv('RTT_CC'):
 
 if  CROSS_TOOL == 'gcc':
     PLATFORM    = 'gcc'
-    EXEC_PATH   = r'/home/gcc-arm-none-eabi-5_4-2016q3/bin'
+    EXEC_PATH   = r'/tools/gcc/bin'
 else:
     print 'Please make sure your toolchains is GNU GCC!'
     exit(0)
