029ca89203084be95e04bd4c060c4103e511ed03
Daniel Doña <daniel-dona@users.noreply.github.com>
2025-09-30 18:08:40 +0200

    Fix

--- rt-thread/components/dfs/include/dfs_select.h
+++ rt-thread/components/dfs/include/dfs_select.h
@@ -7,6 +7,9 @@
 extern "C" {
 #endif
 
+/* Forward declaration for struct timeval */
+struct timeval;
+
 int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
 
 #ifdef __cplusplus
